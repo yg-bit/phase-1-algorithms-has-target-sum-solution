0cba91c4ca7a8b6f08f1f2ff89dcf81419f40937
yg-bit <jamesisaiah164@gmail.com>
2025-09-27 15:44:48 +0300

    added

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // We'll use a Set to keep track of numbers we've seen so far
+  const seenNumbers = new Set();
+
+  // Loop through each number in the array
+  for (const number of array) {
+    // Find the "complement" number that would add up to the target
+    const complement = target - number;
+
+    // If we've already seen the complement, we found a pair!
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
+
+    // Otherwise, add the current number to the set and continue
+    seenNumbers.add(number);
+  }
+
+  // If no pair adds up to target, return false
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Big O Time Complexity:
+  - Time: O(n) because we loop through the array once.
+  - Space: O(n) because we may store up to n numbers in the Set.
 */
 
 /* 
-  Add your pseudocode here
+  Pseudocode:
+  - Create an empty Set called seenNumbers
+  - For each number in the array:
+      - Compute complement = target - number
+      - If complement is in seenNumbers, return true
+      - Otherwise add number to seenNumbers
+  - If loop ends without finding a pair, return false
 */
 
 /*
-  Add written explanation of your solution here
+  Explanation:
+  The idea is to check if we've already seen the number that pairs
+  with the current number to reach the target. 
+  Example: If target = 10 and current number = 7, we need 3.
+  If we've already seen 3, return true. If not, store 7 in the set.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
